7e57767b039d78083450cd816540c40c98f6dcca
Alex Trost <alexxtrostt@gmail.com>
2014-12-08 10:49:06 -0800

    updated gem file

--- Eeek/app/controllers/users_controller.rb
+++ Eeek/app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ApplicationController
+  before_filter :authenticate_user!
 
   def index
   end
 
   def create
+  end
+
+  def show
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to :back, :alert => "Whoops! Please login to see this page."
+    end
   end
 
   def update
