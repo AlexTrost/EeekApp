fa12b769ffe4d3f2d558e8a2a03fa5cc73779bbf
Alex Trost <alexxtrostt@gmail.com>
2014-12-09 13:15:44 -0800

    added javascript animation to front page

---
+++ Eeek/app/assets/javascripts/homepage.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function() {
+	
+
+	$(".black_arrow").on("click", function() {
+		$(".wide").animate({"margin-left": "-100%"}, 900) });
+
+	$(".black_arrow2").on("click", function() {
+		$(".wide").animate({"margin-left": "0%"}, 900) });
+
+})
