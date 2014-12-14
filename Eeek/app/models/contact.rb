e80338959573f16f1861f232b1af68094fac7e9a
Alex Trost <alexxtrostt@gmail.com>
2014-12-14 11:22:54 -0800

    added contact validations

--- Eeek/app/models/contact.rb
+++ Eeek/app/models/contact.rb
@@ -1,2 +1,7 @@
 class Contact < ActiveRecord::Base
+	validates :contact_name, presence: true
+  validates :phone_number, presence: true
+  validates :phone_number, format: { with: /\A\d{10}\z/i}
+
+	belongs_to :user
 end
--- Eeek/app/models/user.rb
+++ Eeek/app/models/user.rb
@@ -4,11 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # mount_uploader :datafile, DatafileUploader       
+  has_many :contacts
 
-   # validates :name, presence: true
-   # validates :email, presence: true
-   # validates :email, uniqueness: true
-   # validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
-   # validates :password, presence: true
+
 end
