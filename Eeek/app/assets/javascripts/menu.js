ec8ab410c42d9da2a834d2aeec9e7de5dad0a1b1
Alex Trost <alexxtrostt@gmail.com>
2014-12-19 13:33:18 -0700

    new uploads form now disapears upon submit

--- Eeek/app/assets/javascripts/menu.js
+++ Eeek/app/assets/javascripts/menu.js
@@ -4,4 +4,8 @@ $(document).ready(function() {
 		$(".new_contact_form_div").remove()
 	})
 
+	$(".render_form_target_div").on("submit", "#new_upload_form", function(){
+		$(".new_upload_form_div").remove()
+	})
+
 	})
