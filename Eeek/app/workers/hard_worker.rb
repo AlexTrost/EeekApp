214a490a2ab365906446aae088aa9fb58796a5aa
Alex Trost <alexxtrostt@gmail.com>
2015-01-28 10:33:31 -0800

    got rid of some old code that isnt needed

--- Eeek/app/workers/hard_worker.rb
+++ Eeek/app/workers/hard_worker.rb
@@ -1,16 +1,3 @@
-# class HardWorker
-#   include Sidekiq::Worker
-
-
-#   # def feed_stuff_back
-#   #   p "hello"
-#   # end
-
-#   def perform(name, count)
-#     puts 'Doing hard work'
-#   end
-# end
-
 class HardWorker
   include Sidekiq::Worker
   include Sidetiq::Schedulable
