e3521cd5ca84718b275270762f5a4586d74abbc2
Alex Trost <alexxtrostt@gmail.com>
2015-02-24 10:08:52 -0800

    slight change to css tree

--- Eeek/app/assets/javascripts/application.js
+++ Eeek/app/assets/javascripts/application.js
@@ -13,4 +13,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require jquery-ui
+//= require jquery-ui/effect.all
+//= require jquery-ui/effect-bounce
+//= require owl.carousel
+
+
 //= require_tree .
