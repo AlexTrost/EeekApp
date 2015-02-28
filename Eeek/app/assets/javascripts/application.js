19843ada940cfac18178d6f0c13199fc61d6cf36
Alex Trost <alexxtrostt@gmail.com>
2015-02-28 13:52:51 -0800

    just had to restart the server. annoyed with self.

--- Eeek/app/assets/javascripts/application.js
+++ Eeek/app/assets/javascripts/application.js
@@ -11,8 +11,11 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery_ujs
 //= require turbolinks
 //= require jquery-ui
+//= require jquery-ui/effect.all
+//= require jquery-ui/effect-bounce
 //= require owl.carousel
 
 
