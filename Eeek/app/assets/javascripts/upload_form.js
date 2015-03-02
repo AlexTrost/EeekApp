b882857f2aad266ead62c355d07e9eba1c83a08c
Alex Trost <alexxtrostt@gmail.com>
2015-03-02 10:14:41 -0800

    js for form ugly but fully functional

--- Eeek/app/assets/javascripts/upload_form.js
+++ Eeek/app/assets/javascripts/upload_form.js
@@ -2,18 +2,17 @@ $(document).ready(function() {
 	
 
 	$("body").on("click", "#text_checkbox", function() {
-		alert("hi")
-		$("#new_upload_form").fadeIn()
-		$("#new_text_form").fadeOut()
-		$("#upload_checkbox").css("background-color", "yellow")
-		$("#text_checkbox").css("background-color", "none")
+		$("#new_text_form").fadeIn()
+		$("#new_upload_form").hide()
+		$("#text_checkbox").css("background-color", "yellow")
+		$("#upload_checkbox").css("background-color", "transparent")
 	})
 
 	$("body").on("click", "#upload_checkbox", function() {
-		$("#new_text_form").fadeIn()
-		$("#new_upload_form").fadeOut()
-		$("#text_checkbox").css("background-color", "yellow")
-		$("#upload_checkbox").css("background-color", "none")
+		$("#new_upload_form").fadeIn()
+		$("#new_text_form").hide()
+		$("#upload_checkbox").css("background-color", "yellow")
+		$("#text_checkbox").css("background-color", "transparent")
 
 	})
 	
