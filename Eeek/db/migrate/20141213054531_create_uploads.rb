190496f022aa1ca8a2143946028be2debfb6bedd
Alex Trost <alexxtrostt@gmail.com>
2015-03-01 17:26:55 -0800

    added text field to uploads table

--- Eeek/db/migrate/20141213054531_create_uploads.rb
+++ Eeek/db/migrate/20141213054531_create_uploads.rb
@@ -2,6 +2,7 @@ class CreateUploads < ActiveRecord::Migration
   def change
     create_table :uploads do |t|
       t.string :name
+      t.text :text
       t.string :attachment
       t.references :user
       t.timestamps
