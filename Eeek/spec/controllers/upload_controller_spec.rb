060a509e560bc3332c10f5fa39b87b933d2bce50
Alex Trost <alexxtrostt@gmail.com>
2015-03-05 09:24:02 -0800

    wrote more passing tests for upload controller

--- Eeek/spec/controllers/upload_controller_spec.rb
+++ Eeek/spec/controllers/upload_controller_spec.rb
@@ -2,15 +2,23 @@ require 'rails_helper'
 
 RSpec.describe UploadsController, :type => :controller do
 
-	# describe 'upload#index' do
- #    it 'returns a succesful status' do 
- #      get :index
- #      expect(response.status).to eq(200)
- #    end
- #    it 'renders the correct html page' do
- #      get :index
- #      expect(response).to render_template("index")
- #    end
- #  end
+	describe 'upload#index' do
+		login_user
+    it 'returns a succesful status' do 
+      get :index
+      expect(response.status).to eq(200)
+    end
+    it 'renders the correct html page' do
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
+
+  describe "upload#index (not logged in)" do 
+  	it "returns a redirect status" do 
+  		get :index
+  		expect(response.status).to eq(302)
+  	end
+  end
 
 end
