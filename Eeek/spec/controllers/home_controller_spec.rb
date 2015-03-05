b640103ab2995717f50b99f0c4718e4b322bfe99
Alex Trost <alexxtrostt@gmail.com>
2015-03-05 09:13:53 -0800

    wrote 2 more passings tests

--- Eeek/spec/controllers/home_controller_spec.rb
+++ Eeek/spec/controllers/home_controller_spec.rb
@@ -2,4 +2,19 @@ require 'rails_helper'
 
 RSpec.describe HomeController, :type => :controller do
 
+	describe "home#index" do 
+		login_user
+		it "returns successful status if user is logged in" do 
+			get :index
+			expect(response.status).to eq(200)
+		end
+	end
+
+	describe 'home#index (not logged in)' do 
+		it "returns a redirect if user isn't logged in" do 
+			get :index
+			expect(response.status).to eq(302)
+		end
+	end
+
 end
