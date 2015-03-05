ef886658cd35ddbc44d3cdd4e6d59bd3b9da3b91
Alex Trost <alexxtrostt@gmail.com>
2015-03-05 10:16:27 -0800

    teststsetstests

--- Eeek/spec/controllers/upload_controller_spec.rb
+++ Eeek/spec/controllers/upload_controller_spec.rb
@@ -2,31 +2,6 @@ require 'rails_helper'
 
 RSpec.describe UploadsController, :type => :controller do
 
-	describe 'upload#index' do
-		login_user
-    it 'returns a succesful status' do 
-      get :index
-      expect(response.status).to eq(200)
-    end
-    it 'renders the correct html page' do
-      get :index
-      expect(response).to render_template("index")
-    end
-    it 'assigns @uploads to Uploads.all' do 
-    	get :index
-    	expect(assigns(:uploads)).to eq Upload.where(user_id: 1) #unsure how to reference id of user logged in. might need to change this later if another user is added to test.
-    end
-    it 'assigns @user_uploads to all uploads uploaded by user' do 
-    	get :index
-    	expect(assigns(:user_uploads)).to eq Upload.where(complimentary: nil)
-  	end
-  	it 'assigns @complimentary_uploads '
-
- 
-
-
-  end
-
   describe "when not logged in" do 
   	it "upload#index returns a redirect status" do 
   		get :index
@@ -45,6 +20,38 @@ RSpec.describe UploadsController, :type => :controller do
   	# 	expect(response.status).to eq(302)
   	# end
   end
+	
+	describe 'upload#index' do
+		login_user
+    it 'returns a succesful status' do 
+      get :index
+      expect(response.status).to eq(200)
+    end
+    it 'renders the correct html page' do
+      get :index
+      expect(response).to render_template("index")
+    end
+    it 'assigns @uploads to Uploads.all' do 
+    	get :index
+    	expect(assigns(:uploads)).to eq Upload.where(user_id: 1) #unsure how to reference id of user logged in. might need to change this later if another user is added to test.
+    end
+    it 'assigns @user_uploads to all uploads uploaded by user' do 
+    	get :index
+    	expect(assigns(:user_uploads)).to eq Upload.where(complimentary: nil) #this test only works with one user in the test database.
+  	end
+  	it 'assigns @complimentary_uploads to all complimentary uploads' do 
+  		get :index
+ 			expect(assigns(:complimentary_uploads)).to eq Upload.where(complimentary: "true") #this test only works with one user in the test database.
+ 		end
+ 		it 'displays the text uploads' do
+      @upload = Upload.create(name: "hey", text: "howdy")
+      get :index
+      response.body { should have_selector('p', text: @upload.text) }
+    end
+    it 'displays the image uploads' do 
+    	@upload = Upload.create(name:"image", attatchment: )
+  end
+
 
   describe "upload#new" do
 		login_user 
