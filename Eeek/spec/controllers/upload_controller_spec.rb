44a831f7e5d4b6d6e90471ed0d58d93f4c8e0193
Alex Trost <alexxtrostt@gmail.com>
2015-03-05 09:56:47 -0800

    tests for upload index coming swimminly

--- Eeek/spec/controllers/upload_controller_spec.rb
+++ Eeek/spec/controllers/upload_controller_spec.rb
@@ -12,6 +12,19 @@ RSpec.describe UploadsController, :type => :controller do
       get :index
       expect(response).to render_template("index")
     end
+    it 'assigns @uploads to Uploads.all' do 
+    	get :index
+    	expect(assigns(:uploads)).to eq Upload.where(user_id: 1) #unsure how to reference id of user logged in. might need to change this later if another user is added to test.
+    end
+    it 'assigns @user_uploads to all uploads uploaded by user' do 
+    	get :index
+    	expect(assigns(:user_uploads)).to eq Upload.where(complimentary: nil)
+  	end
+  	it 'assigns @complimentary_uploads '
+
+ 
+
+
   end
 
   describe "when not logged in" do 
@@ -27,10 +40,10 @@ RSpec.describe UploadsController, :type => :controller do
   		get :create
   		expect(response.status).to eq(302)
   	end
-  	it "upload#destroy returns a redirect status" do 
-  		get :destroy
-  		expect(response.status).to eq(302)
-  	end
+  	# it "upload#destroy returns a redirect status" do 
+  	# 	get :destroy
+  	# 	expect(response.status).to eq(302)
+  	# end
   end
 
   describe "upload#new" do
@@ -39,6 +52,10 @@ RSpec.describe UploadsController, :type => :controller do
   		xhr :get, :new
 			expect(response.status).to eq(200)
   	end
+  	# it "creates a new text upload with valid params" do 
+  	# 	get :new, {name: "test", text: "this is a test"}
+  	# 	expect(Upload.count).to change_by(1)
+  	# end
   end
 
 end
