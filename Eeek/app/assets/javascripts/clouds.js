0f506615a16cc5778a40b1fcfd304b3d7013efc3
Alex Trost <alexxtrostt@gmail.com>
2015-03-06 09:42:01 -0800

    going crazy time to take a break

--- Eeek/app/assets/javascripts/clouds.js
+++ Eeek/app/assets/javascripts/clouds.js
@@ -5,19 +5,20 @@ $(document).ready(function() {
 	for (var i = 0; i < all_clouds.length; i++) {
 
 		
-		cloudAnimation()
+		cloudAnimation(i)
 
-		function cloudAnimation() {
-			
+		function cloudAnimation(i) {
 		var num = Math.floor(Math.random() * (40 - 900) + 3)
 		var speed = num * num
 		var $cloud = $(all_clouds[i])
+		// var cloud_number = $cloud.attr('class').slice(-1)
+		console.log($cloud.attr('class'))
 		var $cloud_hight = $cloud.height()
 		var $page_width = $("body").width()
-			$cloud.animate({"left": $page_width}, speed, "linear");
+			$cloud.animate({"left": 1000}, speed, "linear");
 			$cloud.animate({"opacity": 0}, 10)
 			$cloud.animate({"left": -200}, 100)
-			$cloud.animate({"opacity": 1}, 10, cloudAnimation)
+			$cloud.animate({"opacity": 1}, 10, cloudAnimation(cloud_number))
 		}
 
 	}
