f472d540f1a75cda1efe35b9dd937a059f641755
Alex Trost <alexxtrostt@gmail.com>
2015-03-06 11:24:54 -0800

    works ok

--- Eeek/app/assets/javascripts/clouds.js
+++ Eeek/app/assets/javascripts/clouds.js
@@ -1,17 +1,76 @@
 $(document).ready(function() {
 
-	var $cloud = $(".cloud3")
-	var $cloud_hight = $cloud.height()
-	var $page_width = $("body").width()
-	
-	cloudAnimation()
 
-	function cloudAnimation() {
-		$cloud.animate({"left": $page_width}, 1000, "linear");
-		$cloud.animate({"opacity": 0}, 10)
-		$cloud.animate({"left": -200}, 100)
-		$cloud.animate({"opacity": 1}, 10, cloudAnimation)
+	function cloud1() {
+		var $cloud = $(".cloud1")
+		var $cloud_hight = $cloud.height()
+		var $page_width = $("body").width()
+		cloudAnimation()
+		function cloudAnimation() {
+			$cloud.animate({"left": $page_width}, 180000, "linear");
+			$cloud.animate({"opacity": 0}, 10)
+			$cloud.animate({"left": -200}, 100)
+			$cloud.animate({"opacity": 1}, 10, cloudAnimation)
+		}
 	}
+
+	function cloud2() {
+		var $cloud = $(".cloud2")
+		var $cloud_hight = $cloud.height()
+		var $page_width = $("body").width()
+		cloudAnimation()
+		function cloudAnimation() {
+			$cloud.animate({"left": $page_width}, 100000, "linear");
+			$cloud.animate({"opacity": 0}, 10)
+			$cloud.animate({"left": -200}, 100)
+			$cloud.animate({"opacity": 1}, 10, cloudAnimation)
+		}
+	}
+
+	function cloud3() {
+		var $cloud = $(".cloud3")
+		var $cloud_hight = $cloud.height()
+		var $page_width = $("body").width()
+		cloudAnimation()
+		function cloudAnimation() {
+			$cloud.animate({"left": $page_width}, 120000, "linear");
+			$cloud.animate({"opacity": 0}, 10)
+			$cloud.animate({"left": -200}, 100)
+			$cloud.animate({"opacity": 1}, 10, cloudAnimation)
+		}
+	}
+
+	function cloud4() {
+		var $cloud = $(".cloud4")
+		var $cloud_hight = $cloud.height()
+		var $page_width = $("body").width()
+		cloudAnimation()
+		function cloudAnimation() {
+			$cloud.animate({"left": $page_width}, 190000, "linear");
+			$cloud.animate({"opacity": 0}, 10)
+			$cloud.animate({"left": -200}, 100)
+			$cloud.animate({"opacity": 1}, 10, cloudAnimation)
+		}
+	}
+
+	function cloud5() {
+		var $cloud = $(".cloud5")
+		var $cloud_hight = $cloud.height()
+		var $page_width = $("body").width()
+		cloudAnimation()
+		function cloudAnimation() {
+			$cloud.animate({"left": $page_width}, 900g00, "linear");
+			$cloud.animate({"opacity": 0}, 10)
+			$cloud.animate({"left": -200}, 100)
+			$cloud.animate({"opacity": 1}, 10, cloudAnimation)
+		}
+	}
+
+	cloud1()
+	cloud2()	
+	cloud3()	
+	cloud4()	
+	cloud5()	
 
 })
 
