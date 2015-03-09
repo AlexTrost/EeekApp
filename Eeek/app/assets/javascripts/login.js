9c1ce6ccb947eebe091a9d9516c150fec78d8091
Alex Trost <alexxtrostt@gmail.com>
2015-03-09 08:51:12 -0700

    js for form displaying looks awesome

---
+++ Eeek/app/assets/javascripts/login.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+	$("#signup_form_display_button").on("click", function() {
+		if ($("#signup_form_display_button").html() === "Sign Up") {
+			$(".signup_form").slideDown()
+			$("#signup_form_display_button").html("Hide Form")
+		} else {
+			$(".signup_form").slideUp()
+			$("#signup_form_display_button").html("Sign Up")
+		}	
+	})
+
+
+})
