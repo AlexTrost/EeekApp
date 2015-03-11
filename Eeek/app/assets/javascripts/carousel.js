5d08a85b224ae69c94cc878af95aff8f2f6ba20a
Alex Trost <alexxtrostt@gmail.com>
2015-03-10 18:36:54 -0700

    working on carousel page

--- Eeek/app/assets/javascripts/carousel.js
+++ Eeek/app/assets/javascripts/carousel.js
@@ -5,8 +5,8 @@ $(document).on('ready page:load', function() {
     var items = frames
     owl.owlCarousel({
         items : items, //10 items above 1000px browser width
-        itemsDesktop : [1000,3], //5 items between 1000px and 901px
-        itemsDesktopSmall : [900,2], // betweem 900px and 601px
+        itemsDesktop : [1000,1], //5 items between 1000px and 901px
+        itemsDesktopSmall : [900,1], // betweem 900px and 601px
         itemsTablet: [600,1], //2 items between 600 and 0
         itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
     });
