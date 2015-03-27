5b0a698a8403362c4f9ab249d01ca42bf4956127
Alex Trost <alexxtrostt@gmail.com>
2015-03-26 17:23:16 -0700

    score increases and leaves drop

--- Eeek/app/assets/javascripts/leaf_game.js
+++ Eeek/app/assets/javascripts/leaf_game.js
@@ -2,7 +2,27 @@ $(document).on('ready page:load', function() {
 
 	$(".game_start").on("click", function() {
 		$(this).css("background-color", "red")
-		$(body).append("<div class='click_leaf'></div>")
+		newLeaf()
 	})
 
+	function newLeaf() {
+		$("body").append("<div class='click_leaf'></div>")
+		var random_left = Math.floor((Math.random() * 100) + 1);
+		$(".click_leaf").css("left", random_left + "em")
+		LeafDrop()
+	}
+
+	function LeafDrop() {
+		$('.click_leaf').animate({"top":"100%"}, 2000)
+	}
+
+	$("body").on("click", ".click_leaf", function() {
+		newLeaf()
+		increaseScore()
+	})
+
+	function increaseScore() {
+		var score = parseInt($(".score").html())
+		$(".score").html(score + 1)
+	}
 })
