9c3e14215402ba8496554e7ac3322f55dfe41777
Alex Trost <alexxtrostt@gmail.com>
2015-04-03 17:12:44 -0700

    fog now works

--- Eeek/config/initializers/fog.rb
+++ Eeek/config/initializers/fog.rb
@@ -1,9 +1,12 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AWS_SECRET_KEY_ID"],                        # required
-    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],                        # required
-  }
-  config.fog_directory  = 'eeek_app'                          # required
-  config.fog_public     = false                                        # optional, defaults to true
-end
+# if Rails.env.production? 
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider:              'AWS',                        # required
+      aws_access_key_id:     ENV["AWS_SECRET_KEY_ID"],                        # required
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region:                'US Standard'                          # required
+    }
+    config.fog_directory  = ENV["AWS_BUCKET"]                          # required
+    config.fog_public     = false                                        # optional, defaults to true
+  end
+# end
