24f9e2b180bdc7e45a66dcf430d9256106879b71
Alex Trost <alexxtrostt@gmail.com>
2015-04-09 10:09:49 -0700

    created mood controller'

---
+++ Eeek/app/controllers/moods_controller.rb
@@ -0,0 +1,20 @@
+class MoodController < ApplicationController
+
+	def create
+		params[:user_id] = current_user.id
+    @mood = Mood.new(mood_params)
+		@mood.user_id = current_user.id
+      respond_to do |format|
+        if @mood.save
+          format.js { render :'mood_confirm', locals: { mood: @mood } }
+          format.html { redirect_to :back }
+        else
+          format.js { render :mood_fail }
+          return "Mood save failed"
+        end
+      end
+    end
+  end
+
+
+end
