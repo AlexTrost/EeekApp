cb3cf585dc8b33cb6e4414be0871188cccb2083f
Alex Trost <alexxtrostt@gmail.com>
2015-04-16 11:11:59 -0700

    wrote tests for contact model

--- Eeek/spec/models/contact_spec.rb
+++ Eeek/spec/models/contact_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
-# RSpec.describe Contact, :type => :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Contact, :type => :model do
+
+	it { should belong_to(:user) }
+
+	it { should allow_value("Alex").for(:contact_name) }
+	it { should_not allow_value("").for(:contact_name) }
+
+	it { should allow_value("1234567890").for(:phone_number) }
+	it { should_not allow_value("asdfghjklo").for(:phone_number) }
+	it { should_not allow_value("").for(:phone_number) }
+	it { should_not allow_value("12345678").for(:phone_number) }
+	it { should_not allow_value("12345678901").for(:phone_number) }
+
+end
