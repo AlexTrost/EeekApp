9581eb160ba55c38784cf5265c48cae2f9497af3
Alex Trost <alexxtrostt@gmail.com>
2015-04-16 10:39:59 -0700

    additional model validation tests added for user

--- Eeek/spec/models/user_spec.rb
+++ Eeek/spec/models/user_spec.rb
@@ -6,14 +6,19 @@ RSpec.describe User, :type => :model do
 	  it { should have_many(:contacts) }
 	  it { should have_many(:uploads) }
 	  it { should have_many(:moods) }
+
 	  it { should allow_value("Alex").for(:name) }
 	  it { should allow_value("Alex Trost").for(:name) }
-	  it { should_not allow_value("Alex@alex.alex").for(:email) }
-	  it { should allow_value("Alex").for(:email) }
+
+	  it { should allow_value("Alex@alex.alex").for(:email) }
+	  it { should_not allow_value("Alex").for(:email) }
 	  it { should_not allow_value("Alex@alex").for(:email) }
 	  it { should_not allow_value("Alex.alex").for(:email) }
-	  # it { expect(blog).to allow_blank(:connect_to_facebook) }
-	  # it { expect(blog).to allow_nil(:connect_to_facebook) }
+	  it { should_not allow_value("").for(:email) }
+
+	  it { should_not allow_value("").for(:password) }
+	  it { should_not allow_value("yo").for(:password)}
+	  it { should allow_value("yoyoyoyoyo").for(:password)}
 	end
 
 
